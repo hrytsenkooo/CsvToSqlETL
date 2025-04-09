524d59bb270c7ef9ed3095c1094addcd01320219
Hrytsenko Artem <hrytsenko.artem@lll.kpi.ua>
2025-04-09 12:36:01 +0300

    feat: added TripRecord model for CSV data

---
+++ CsvToSqlETL/Models/TripRecord.cs
@@ -0,0 +1,20 @@
+﻿namespace CsvToSqlETL.Models
+{
+    public class TripRecord
+    {
+        public DateTime PickupDatetime { get; set; }
+        public DateTime DropoffDatetime { get; set; }
+        public int PassengerCount { get; set; }
+        public double TripDistance { get; set; }
+        public string StoreAndFwdFlag { get; set; } = "";
+        public int PULocationID { get; set; }
+        public int DOLocationID { get; set; }
+        public decimal FareAmount { get; set; }
+        public decimal TipAmount { get; set; }
+
+        public override string ToString()
+        {
+            return $"{PickupDatetime} -> {DropoffDatetime}, Passengers: {PassengerCount}, Distance: {TripDistance} mi, Tip: {TipAmount}$";
+        }
+    }
+}
